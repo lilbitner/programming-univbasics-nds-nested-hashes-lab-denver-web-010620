3d2708d01cabef4fc8544c3bbf855002574ce46f
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-04 22:56:45 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -40,16 +40,9 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  literal_aoh = [
-    {:name => "Don Gately", :occupation => "Live-in Staff"},
-    {:name => "Joelle van Dyne", :occupation => "Radio Personality"}, 
-    {:name => "Pat Monteseian", :occupation => "Staff"}, 
-    {:name => "Kate Gompert", :occupation => "None"},
-    {:name => "Bruce Green", :occupation => "Fan of Mildred"}
-    ]
-    
-  literal_aoh[1][:name] = "Joelle van Dyne (PGOAT)"
-  puts literal_aoh 
+
+  local_copy_of_aoh[1][:name] = "Joelle van Dyne (PGOAT)"
+  puts local_copy_of_aoh
   
 
   
